b04efc561fef178ca9613c5238b10eb115ce39fe
MOH RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-06-20 17:02:13 +0700

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // pages & components
 import Home from './pages/Home'
@@ -9,6 +9,17 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
