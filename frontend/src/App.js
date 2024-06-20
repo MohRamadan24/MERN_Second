9e618950b9aaadc83a2b67079a1af26e77d19efb
MOH RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-06-20 09:44:49 +0700

    bruh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,11 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+// pages & components
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
+
+
 function App() {
   return (
     <div className="App">
